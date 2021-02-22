8b5280f95739d454c910898fac0539a079443b39
shishkovda <shishkovda@mail.ru>
2021-02-23 00:33:49 +0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -17,3 +17,17 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.history
     OWNER to postgres;
+    
+-- SEQUENCE: public.hibernate_sequence
+
+-- DROP SEQUENCE public.hibernate_sequence;
+
+CREATE SEQUENCE public.hibernate_sequence
+    INCREMENT 1
+    START 9
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.hibernate_sequence
+    OWNER TO postgres;
